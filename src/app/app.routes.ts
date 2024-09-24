cda3732ba143a00a827dfb90640cc2839ed50955
MalikWhitfield <malikwhitfield29@gmail.com>
2024-09-24 13:44:04 -0400

    add home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { TodoFormComponent } from './todo-form/todo-form.component';
 import { ProductListComponent } from './product-list/product-list/product-list.component';
+import { AppComponent } from './app.component';
 
 export const routes: Routes = [
   { path: 'todo', component: TodoFormComponent },
   { path: 'products', component: ProductListComponent },
+  //   { path: '', redirectTo: '/home', pathMatch: 'full' },
 ];
