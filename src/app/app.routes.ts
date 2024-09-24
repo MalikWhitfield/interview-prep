57c4e2cf58aba47d02c3a2945ab2cf494d414b9c
MalikWhitfield <malikwhitfield29@gmail.com>
2024-09-24 13:41:19 -0400

    add routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { TodoFormComponent } from './todo-form/todo-form.component';
+import { ProductListComponent } from './product-list/product-list/product-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'todo', component: TodoFormComponent },
+  { path: 'products', component: ProductListComponent },
+];
